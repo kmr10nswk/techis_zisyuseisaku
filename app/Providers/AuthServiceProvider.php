ce36df5b853909fa416f872b2c7ef093065bfb10
kmr10nswk <150318880+kmr10nswk@users.noreply.github.com>
2024-04-17 23:09:20 +0900

    240417 Gate機能

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -21,6 +21,21 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->registerPolicies();
+
+        // 商品＋アカウント
+        Gate::define('item_admin', function($admin){
+            return ($admin->policy->item_admin === 1);
+        });
+
+        // 掲示板＋アカウント
+        Gate::define('theread_admin', function($admin) {
+            return ($admin->policy->theread_admin === 1);
+        });
+
+        // 全て
+        Gate::define('all_admin', function($admin){
+            return ($admin->policy->item_admin === 1 && $admin->policy->theread_admin === 1);
+        });
     }
 }
