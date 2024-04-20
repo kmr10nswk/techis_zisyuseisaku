600dc10c2dbc8b23940a0e8900c36d485c584d0a
kmr10nswk <150318880+kmr10nswk@users.noreply.github.com>
2024-04-20 16:13:41 +0900

    240419 Item追加まで

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Item;
+use App\Models\User;
 
 class ItemController extends Controller
 {
@@ -14,7 +15,22 @@ class ItemController extends Controller
     public function index()
     {
         // 商品一覧取得
-        $items = Item::all();
+        $items = Item::query();
+        $items = $items->orderby('id', 'asc')
+            ->paginate(10)->withQueryString();
+
+        // blade整え
+        $c_list = Item::category_list();
+        $t_list = Item::theme_list();
+        $k_list = Item::kind_list();
+        $co_list = Item::company_list();
+
+        foreach ($items as $item){
+            $item->category = $c_list[$item->category];
+            $item->theme = $t_list[$item->theme];
+            $item->kind = $k_list[$item->kind];
+            $item->company = $co_list[$item->company];
+        }
 
         return view('item.index', compact('items'));
     }
@@ -28,20 +44,42 @@ class ItemController extends Controller
         if ($request->isMethod('post')) {
             // バリデーション
             $this->validate($request, [
-                'name' => 'required|max:100',
+                'name' => ['required', 'max:50', 'string'],
+                'category' => ['required', 'between:1,4'],
+                'theme' => ['required', 'between:1,7',],
+                'kind' => ['required', 'between:1,3',],
+                'company' => ['required', 'between:1,4',],
+                'release' => ['required', 'date'],
+                'detail' => ['required', 'max:500', 'string'],
+                'image_item' => ['nullable', 'image', 'mimes:jpeg,png', 'max:1024'],
             ]);
+
+            // 画像処理
+            $file = $request->file('image_item');
+            $file_name = User::uploadImage($file, 'item');
 
             // 商品登録
             Item::create([
-                'user_id' => Auth::user()->id,
                 'name' => $request->name,
-                'type' => $request->type,
+                'category' => $request->category,
+                'theme' => $request->theme,
+                'kind' => $request->kind,
+                'company' => $request->company,
+                'release' => $request->release,
                 'detail' => $request->detail,
+                'image_item' => $file_name,
             ]);
 
             return redirect('/items');
         }
 
-        return view('item.add');
+        // GETリクエストのとき
+        // 各リストを引き出す
+        $category_list = Item::category_list();
+        $theme_list = Item::theme_list();
+        $kind_list = Item::kind_list();
+        $company_list = Item::company_list();
+
+        return view('item.add', compact('category_list', 'theme_list', 'kind_list', 'company_list'));
     }
 }
